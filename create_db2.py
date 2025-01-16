033ba1d5ee0244919afbcf94a3ab211c5ad39869
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-01-16 14:45:06 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -7,14 +7,7 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()  #preparer l injection de lignes
 
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Fiction'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Les Misérables', 'Victor Hugo', 'Roman Historique'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('L\'Etranger', 'Albert Camus', 'Philosophie'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 'Fantastique'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Le Seigneur des Anneaux', 'J.R.R. Tolkien', 'Fantasy'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Da Vinci Code', 'Dan Brown', 'Thriller'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('L\'Art de la Guerre', 'Sun Tzu', 'Stratégie'))  
-cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Les Fleurs du mal', 'Charles Baudelaire', 'Poésie'))  
+
 
 connection.commit()
 connection.close()
