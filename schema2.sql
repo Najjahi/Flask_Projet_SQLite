6bfb8f0c2aaaab6cb33666231a7c194300ac3cae
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-01-16 15:14:41 +0100

    Update schema2.sql

--- schema2.sql
+++ schema2.sql
@@ -3,10 +3,10 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,          -- Titre du livre
     author TEXT NOT NULL,         -- Auteur du livre
-    genre TEXT,                 -- Genre du livre (optionnel)
+    genre TEXT                -- Genre du livre (optionnel)
     --published_date DATE,          -- Date de publication
     --isbn TEXT UNIQUE,             -- ISBN (identifiant unique pour les livres)
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- Date d'enregistrement
+    --created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- Date d'enregistrement
 );
 
 -- Table des utilisateurs
