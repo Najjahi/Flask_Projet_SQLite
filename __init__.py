8dd70a25d218b1ea41c408d2fb45bfc65b77e3ad
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-01-16 11:21:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
+
+USERNAME = 'user'
+PASSWORD = '12345'
+
 # Fonction pour créer une clé "authentifie" dans la session utilisateur
 def est_authentifie():
     return session.get('authentifie')
@@ -77,26 +81,19 @@ def enregistrer_client():
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
-USERNAME = 'user'
-PASSWORD = '12345'
 
-# Page de connexion
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Vérification des identifiants
-        if username == USERNAME and password == PASSWORD:
-            session['logged_in'] = True
-            return redirect(url_for('home'))
-        else:
-            return "Nom d'utilisateur ou mot de passe incorrect.", 403
-
-    return redirect('/fiche_client/')
 @app.route('/fiche_client/<string:nom>')
-def fiche_client(nom):    
+ if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('lecture'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+
+   #return render_template('formulaire_authentification.html', error=False)
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
