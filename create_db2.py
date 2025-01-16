f9d77212838b2c67248953e2dda2b5264449399c
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-01-16 14:56:38 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -7,6 +7,14 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()  #preparer l injection de lignes
 
-
+Le Petit Prince	Antoine de Saint-Exupéry	Fiction
+3	Les Misérables	Victor Hugo	Roman Historique
+4	L'Étranger	Albert Camus	Philosophie
+5	Harry Potter à l'école des sorciers	J.K. Rowling	Fantastique
+6	Le Seigneur des Anneaux	J.R.R. Tolkien	Fantasy
+7	Da Vinci Code	Dan Brown	Thriller
+8	L'Art de la Guerre	Sun Tzu	Stratégie
+9	La Peste	Albert Camus	Roman
+10	Les Fleurs du mal	Charles Baudelaire	Poésie
 connection.commit()
 connection.close()
