ce1994ec22c538fb3e5efa8916dc1f540841a81e
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-01-16 15:12:44 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -7,14 +7,14 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()  #preparer l injection de lignes
 
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('Les Miserables', 'Victor Hugo', 'Roman Historique'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Fiction'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('Le Seigneur des Anneaux', 'J.R.R. Tolkien', '789, Fantasy'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('Da Vinci Code', 'Dan Brown', 'Thriller'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('La Peste', 'Albert Camus', 'Roman'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('Les Fleurs du mal', 'Charles Baudelaire', 'Poesie'))
-#cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
-#cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
+cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Les Miserables', 'Victor Hugo', 'Roman Historique'))
+cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Fiction'))
+cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Le Seigneur des Anneaux', 'J.R.R. Tolkien', '789, Fantasy'))
+cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Da Vinci Code', 'Dan Brown', 'Thriller'))
+cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('La Peste', 'Albert Camus', 'Roman'))
+cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('Les Fleurs du mal', 'Charles Baudelaire', 'Poesie'))
+#cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
+#cur.execute("INSERT INTO books (title, author, genre) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
 
 connection.commit()
